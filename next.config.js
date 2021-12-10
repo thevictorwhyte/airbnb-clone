8b81d41a4d6adf2277f75bf5cd978db7ea5c2532
thevictorwhyte <victordavidwhyte@gmail.com>
2021-12-11 00:17:39 +0100

    map functionality using mapbox

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   images: {
-    domains: ['links.papareact.com'],
+    domains: ["links.papareact.com"],
   },
-}
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoidGhldmljdG9yd2h5dGUiLCJhIjoiY2t4MHkwamM0MHdlbzJ4b2IzMDEwMHJobiJ9.wkgwpQfsl9bZCE4fEXoFdQ",
+  },
+};
